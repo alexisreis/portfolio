6536c6893475092e5071eeda112778e88bd79d09
Alexis Reis <areis@tcd.ie>
2024-08-29 12:42:44 +0200

    feat: package update * fix NextLinks with <a> * fix Images width and heights

--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import { useState } from 'react';
-import NextLink from 'next/link';
+import Link from './Link';
 
 const LanguageSwitcher = () => {
   const router = useRouter();
@@ -39,11 +39,15 @@ const LanguageSwitcher = () => {
         <div className="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-10">
           <div role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
             {locales.map((locale) => (
-              <NextLink key={locale} href={router.asPath} locale={locale} onClick={closeMenu}>
-                <a className="flex flex-1 rounded-md px-4 py-2 text-base text-gray-700 hover:bg-gray-100 hover:text-gray-900">
-                  {flags[locale] + ' ' + languages[locale]}
-                </a>
-              </NextLink>
+              <Link
+                key={locale}
+                className="flex flex-1 rounded-md px-4 py-2 text-base text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                href={router.asPath}
+                locale={locale}
+                onClick={closeMenu}
+              >
+                {flags[locale] + ' ' + languages[locale]}
+              </Link>
             ))}
           </div>
         </div>
