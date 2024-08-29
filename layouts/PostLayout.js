c5baf48b2b43d7e520f803def86c29a083469512
Alexis Reis <areis@tcd.ie>
2024-08-29 12:39:02 +0200

    Clean up * remove unused icons * remove unused and deprecated scripts * some css fixes

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -6,15 +6,7 @@ import formatDate from '@/lib/utils/formatDate';
 import Image from '@/components/Image';
 
 export default function PostLayout({ frontMatter, authorDetails, next, prev, children }) {
-  const { date, title, slug, fileName, readingTime } = frontMatter;
-
-  const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/master/data/blog/${fileName}`;
-  const discussUrl = (slug) =>
-    `https://mobile.twitter.com/search?q=${encodeURIComponent(
-      `${siteMetadata.siteUrl}/blog/${slug}`
-    )}`;
-
-  const pageViews = undefined;
+  const { date, title, readingTime } = frontMatter;
 
   return (
     <>
@@ -37,14 +29,13 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
             </div>
             <div className="flex items-center justify-between">
               <div>
-                {authorDetails.map((author) => (
-                  <Link key={author.name} href={author.twitter}>
+                {authorDetails.map((author) => (<>
                     <div className="mb-1 flex items-center space-x-2">
                       {author.avatar && (
                         <Image
                           src={author.avatar}
-                          width="24px"
-                          height="24px"
+                          width={24}
+                          height={24}
                           alt="avatar"
                           className="h-10 w-10 rounded-full"
                         />
@@ -52,27 +43,19 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                       <dl className="whitespace-nowrap text-sm font-medium leading-5">
                         <dt className="sr-only">Name</dt>
                         <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                        <dt className="sr-only">Twitter</dt>
                       </dl>
                     </div>
-                  </Link>
+                  </>
                 ))}
               </div>
               <div className="flex items-center space-x-2 ">
                 <div className="text-gray-500 dark:text-gray-400">{readingTime.text}</div>
-                {pageViews && <div>Page Views</div>}
               </div>
             </div>
           </header>
           <div className="pb-8 " style={{ gridTemplateRows: 'auto 1fr' }}>
             <div className=" xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pb-4 dark:prose-dark">{children}</div>
-              <div className="flex justify-between pt-6 text-sm text-gray-700 dark:text-gray-300">
-                <Link href={editUrl(fileName)}>{'Have a nice day 🌞'}</Link>
-                <Link href={discussUrl(slug)} rel="nofollow">
-                  {'Discuss on Twitter'}
-                </Link>
-              </div>
             </div>
             <footer>
               <div className="flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base">
@@ -87,7 +70,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                   </div>
                 )}
                 {next && (
-                  <div className="pt-4 xl:pt-8">
+                  <div className="pt-4 text-right xl:pt-8">
                     <Link
                       href={`/blog/${next.slug}`}
                       className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
