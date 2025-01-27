04d40b4225014e097ae0da4af6934778ca558c40
Alexis Reis <areis@tcd.ie>
2025-01-27 16:21:25 +0100

    feat: ReadMore (WIP) * Card and ListItem styles * workxp links to blog articles

--- data/workxp.js
+++ data/workxp.js
@@ -11,13 +11,13 @@ export const workxp = (i18n) => [
     img: '/static/images/workxp/worldline.png',
     dates: i18n.t('home.workxp.worldline.dates'),
     description: i18n.t('home.workxp.worldline.description'),
-    href: 'https://worldline.com',
+    href: '/projects/2023-08-Resapp',
   },
   {
     title: i18n.t('home.workxp.liris.title'),
     img: '/static/images/workxp/liris.png',
     dates: i18n.t('home.workxp.liris.dates'),
     description: i18n.t('home.workxp.liris.description'),
-    href: 'https://liris.cnrs.fr',
+    href: '/projects/2022-08-annotation-app',
   },
 ];
