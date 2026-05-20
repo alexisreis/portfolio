a655808bfac08b90e2585f91c53fa6ec9b10f834
Alexis Reis <alexisreis@outlook.fr>
2026-05-20 13:47:05 +0200

    fix: button size varing on small screen and items

--- components/Buttons.js
+++ components/Buttons.js
@@ -3,7 +3,7 @@ import Link from '@/components/Link';
 export const StrokeButton = ({ text, href }) => {
   return (
     <Link
-      className="my-auto ml-auto flex rounded-xl border border-primary-500 px-4 py-2 text-center font-medium text-primary-500 transition duration-300 ease-in-out hover:bg-primary-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-opacity-50 dark:hover:border-primary-700"
+      className="my-auto ml-auto flex shrink-0 whitespace-nowrap rounded-xl border border-primary-500 px-4 py-2 text-center font-medium text-primary-500 transition duration-300 ease-in-out hover:bg-primary-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-opacity-50 dark:hover:border-primary-700"
       href={href}
     >
       {text}
